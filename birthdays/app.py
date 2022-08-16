bcc934df1d6ba8bd903ec3ad36fb580ab54a639a
33patburke <33patburke@gmail.com>
2022-08-15 21:01:39 -0400

    web app fully functional although not pretty

--- birthdays/app.py
+++ birthdays/app.py
@@ -12,28 +12,36 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
-
+# Ensure responses aren't cached
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-
+# Define primary route
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        # Request form data from user into flask backend
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        # Insert the user's entry into the database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        # Go back to homepage
         return redirect("/")
 
     else:
 
-        # TODO: Display the entries in the database on index.html
+        # Query for all birthdays
+        birthdays = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
+        # Render birthdays page
+        return render_template("index.html", birthdays=birthdays)
 
 
